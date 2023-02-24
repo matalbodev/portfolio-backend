4e7e59d01291eb41e9fc684db0c992c45ee48db8
matalbodev <mathieualbore@gmail.com>
2023-02-24 08:48:54 +0100

    Update config deploy heroku

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,10 +1,9 @@
+// Path: ./config/env/production/server.js
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  proxy: true,
+  url: env("MY_HEROKU_URL"), // Sets the public URL of the application.
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    keys: env.array("APP_KEYS"),
   },
 });
